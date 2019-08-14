e626b785cdd3d59aa5135c8cea54f51a3ad116c3
Marco <ma.monzon@soprasteria.com>
2019-08-15 01:01:29 +0200

    feat: diamond embeded

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://www.stickpng.com/assets/images/580b585b2edbce24c47b23f2.png'),
+            width: 200,
+          ),
+        ),
       ),
     ),
   );
